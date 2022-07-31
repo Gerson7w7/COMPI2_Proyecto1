8fe167cd37cc88bed0570adda31c5b67ea67ce01
Gerson <3005563490101@ingenieria.usac.edu.gt>
2022-07-30 22:49:36 -0600

    siuu

--- backend/interprete/analizador/parser.py
+++ backend/interprete/analizador/parser.py
@@ -1,3 +1,4 @@
+# ejemplos: https://www.dabeaz.com/ply/ply.html#ply_nn33
 from ply.yacc import yacc
 from analizador import lexer
 
@@ -14,21 +15,18 @@ def inicio(p):
     """
     inicio : instrucciones
     """
-    #p[0] = Ast(p[1]);
+    p[0] = Ast(p[1]);
 
 # lista de instrucciones
 def instrucciones(p):
     """
     instrucciones : instrucciones instruccion
+        | instruccion
     """
-    p[0] = p[1].append(p[2]);
-
-# la primera instruccion y donde se crea la lista de instrucciones
-def instrucciones2(p):
-    """
-    instrucciones : instruccion
-    """
-    p[0] = [p[1]];
+    if (len(p) == 3):
+        p[0] = p[1].append(p[2]);
+    elif (len(p) == 2):
+        p[0] = [p[1]];     
 
 # declaracion de variables
 def instruccion(p):
@@ -37,8 +35,36 @@ def instruccion(p):
     """
     p[0] = p[1];
 
-def declaracion(p):
+def declaracion1(p):
     """
     declaracion : LET MUT IDENTIFICADOR DOS_PUNTOS type igualacion PUNTO_COMA
-        | LET MUT IDENTIFICADOR igualacion PUNTO_COMA
+    """
+    p[0] = Declaracion(p[5], p[6], p.lineno(1), p.lexpos(1));
+
+def declaracion2(p):
+    """
+    declaracion : LET MUT IDENTIFICADOR igualacion PUNTO_COMA
+    """
+    p[0] = Declaracion(None, p[4], p.lineno(1), p.lexpos(1));
+
+def declaracion3(p):
+    """
+    declaracion : LET IDENTIFICADOR DOS_PUNTOS type igualacion PUNTO_COMA
+    """
+    p[0] = Declaracion(p[4], p[5], p.lineno(1), p.lexpos(1));
+
+def declaracion4(p):
+    """
+    declaracion : LET IDENTIFICADOR igualacion PUNTO_COMA
+    """
+    p[0] = Declaracion(None, p[3], p.lineno(1), p.lexpos(1));
+
+def type(p):
+    """
+    type : I64
+        | F64
+        | BOOL  
+        | CHAR
+        | STRING
+        | AMPERSON STR
     """
